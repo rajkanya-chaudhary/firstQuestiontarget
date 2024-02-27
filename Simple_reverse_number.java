59d7d20acbc6f88ab83fdaebfe3bdeeef80ab906
rajkanya-chaudhary <dc5068786@gmail.com>
2024-02-27 23:10:01 +0530

    this code is a simlpe reverse code

---
+++ Simple_reverse_number.java
@@ -0,0 +1,14 @@
+public class Simple_reverse_number {
+    public static void main(String[] args) {
+        int  n = 12345;
+        int rev = 0, rem;
+        while(n>0)
+        {
+            rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        System.out.println("Reverse number = " + rev);
+
+    }
+}
